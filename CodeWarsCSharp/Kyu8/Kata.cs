b18f8571b085c75aa6129d087cb03e4bb7e1a038
Nathan <dewittnathan@gmail.com>
2023-10-01 13:28:58 +0200

    Between method refactor

--- CodeWarsCSharp/Kyu8/Kata.cs
+++ CodeWarsCSharp/Kyu8/Kata.cs
@@ -2,9 +2,6 @@
 {
     internal class Kata
     {
-        public static int[] Between(int a, int b)
-        {
-            return Enumerable.Range(a, b - a + 1).ToArray();
-        }
+        public static int[] Between(int a, int b) => Enumerable.Range(a, b - a + 1).ToArray();
     }
 }
