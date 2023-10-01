95068a56f6d07336e8cf6b9e7fa2d77a4b1d75ff
Nathan <dewittnathan@gmail.com>
2023-10-01 17:02:45 +0200

    GetPins kata challenge solution added

--- CodeWarsCSharp/Kyu4/Kata.cs
+++ CodeWarsCSharp/Kyu4/Kata.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodeWarsCSharp.Kyu4
+﻿namespace CodeWarsCSharp.Kyu4
 {
     internal class Kata
     {
+        public static List<string> GetPINs(string observed)
+        {
+            Dictionary<char, string> adjacentDigits = new Dictionary<char, string>
+            {
+                ['1'] = "124",
+                ['2'] = "1235",
+                ['3'] = "236",
+                ['4'] = "1457",
+                ['5'] = "24568",
+                ['6'] = "3569",
+                ['7'] = "478",
+                ['8'] = "57890",
+                ['9'] = "689",
+                ['0'] = "08"
+            };
+
+            return observed
+                .Select(digit => adjacentDigits[digit])
+                .Aggregate(Enumerable.Empty<string>(), (acc, next) =>
+                    from a in acc.DefaultIfEmpty("") from n in next select a + n)
+                .ToList();
+        }
     }
 }
--- CodeWarsCSharp/Kyu4/KataTest.cs
+++ CodeWarsCSharp/Kyu4/KataTest.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodeWarsCSharp.Kyu4
+﻿namespace CodeWarsCSharp.Kyu4
 {
     internal class KataTest
     {
+        [TestCase("8", new[] { "5", "7", "8", "9", "0" })]
+        [TestCase("11", new[] { "11", "22", "44", "12", "21", "14", "41", "24", "42" })]
+        [TestCase("369", new[] { "339", "366", "399", "658", "636", "258", "268", "669", "668", "266", "369", "398", "256", "296", "259", "368", "638", "396", "238", "356", "659", "639", "666", "359", "336", "299", "338", "696", "269", "358", "656", "698", "699", "298", "236", "239" })]
+        [Test]
+        public void TestBasic(string observedPin, string[] expectedVariationArr)
+        {
+            // Act
+            var pinResult = Kata.GetPINs(observedPin);
+
+            // Assert
+            CollectionAssert.AreEquivalent(expectedVariationArr, pinResult);
+        }
     }
 }
